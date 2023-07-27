a00069046cbbe07276c40876d88d628c709c4288
kasthuriarachchi996 <www.pasindudenuka5770@gmail.com>
2023-07-27 14:10:12 +0530

    create micro service

---
+++ discovery/src/test/java/com/pasindu/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasindu.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/pasindu/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasindu.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
